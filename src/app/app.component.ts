913b62df5815687d3c6d599710c99705db0deec4
Definkle <52527078+Definkle@users.noreply.github.com>
2020-05-03 18:58:40 +0800

    Intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f', { static: true }) signUpForm: NgForm;
+  submitted = false;
+  defaultSubscription = 'Advanced';
+  user = {
+    subscription: '',
+    email: '',
+    password: '',
+  }
+  onSubmit() {
+    this.submitted = true;
+    this.user.subscription = this.signUpForm.value.userData.subscription;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.password = this.signUpForm.value.userData.password;
+    console.log(this.signUpForm.value.userData);
+    this.signUpForm.reset();
+  }
+}
